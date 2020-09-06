83adb2da18f4f63958bd3105c1a63a5a1432d0f9
riyaz <riyazpasha880@gmail.com>
2020-09-06 11:14:18 +0530

    added comments on array operations:

--- variables.sh
+++ variables.sh
@@ -4,10 +4,10 @@ echo -$Name-
 NAME[2]="Pasha"
 NAME[1]="Riyaz"
 NAME[0]="Mohammed"
-echo -$NAME-
-echo -${NAME[*]}-
-echo -${NAME[@]}-
-for name in $NAME
+echo -$NAME- #prints array[0]
+echo -${NAME[*]}- #prints entire name
+echo -${NAME[@]}- #prints entire name
+for name in $NAME #prints only array[0]
 do
     echo name -$name-
 done
