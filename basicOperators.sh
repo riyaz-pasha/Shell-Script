70ba7ae0c3554da1e78cadf24149cfff057f840e
riyaz <riyazpasha880@gmail.com>
2020-09-06 11:45:22 +0530

    basic equality check

--- basicOperators.sh
+++ basicOperators.sh
@@ -7,3 +7,6 @@ echo "sum :" `expr $firstNumber + $secondNumber`
 echo "sub :" `expr $firstNumber - $secondNumber`
 echo "mul :" `expr $firstNumber \* $secondNumber`
 echo "div :" `expr $firstNumber / $secondNumber`
+
+echo "is Equal : " $[ $firstNumber == $secondNumber ]
+echo "is Not Equal : " $[ $firstNumber != $secondNumber ]
